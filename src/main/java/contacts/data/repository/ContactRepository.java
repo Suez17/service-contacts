d004d2bfa1eec26c4fcbf26dadb6f2a8a2ffab78
mkouyoumdjian <mkouyoumdjian@datanumia.com>
2023-01-28 19:00:21 +0100

    add spring data rest and new queries

--- src/main/java/contacts/data/repository/ContactRepository.java
+++ src/main/java/contacts/data/repository/ContactRepository.java
@@ -1,9 +1,27 @@
 package contacts.data.repository;
 
 import contacts.data.model.ContactEntity;
+import java.util.List;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface ContactRepository extends JpaRepository<ContactEntity, UUID> {
+@RepositoryRestResource(collectionResourceRel = "contacts", path = "contacts")
+public interface ContactRepository extends CrudRepository<ContactEntity, UUID> {
 
+  List<ContactEntity> findByFirstNameContains(@Param("firstName") String firstName);
+
+  List<ContactEntity> findByLastNameContains(@Param("lastName") String lastName);
+
+  List<ContactEntity> findByAddressContains(@Param("address") String address);
+
+  List<ContactEntity> findByFirstNameContainsAndLastNameContains(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
+  List<ContactEntity> findByFirstNameContainsAndAddressContains(@Param("firstName") String firstName, @Param("address") String address);
+
+  List<ContactEntity> findByLastNameContainsAndAddressContains(@Param("lastName") String lastName, @Param("address") String address);
+
+  List<ContactEntity> findByFirstNameContainsAndLastNameContainsAndAddressContains(
+      @Param("firstName") String firstName, @Param("lastName") String lastName, @Param("address") String address);
 }
