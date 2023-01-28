5bd31cc302689a5024a47dcc3140923ce594dab6
mkouyoumdjian <mkouyoumdjian@datanumia.com>
2023-01-28 14:52:14 +0100

    add sql test script

---
+++ src/test/resources/sql/insert-contacts.sql
@@ -0,0 +1,2 @@
+INSERT INTO contact (id, first_name, last_name, birth_date, address, email, phone_number)
+VALUES('bfdf4b3d-ede1-472b-94ad-96845a5820da', 'firstName1', 'lastName1', '2021-01-01', 'address1', 'test@mail.com', '0100000000');
