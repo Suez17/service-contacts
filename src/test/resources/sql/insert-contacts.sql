9739b0a570f7bba5cd0017ecd5c0322c6343002d
mkouyoumdjian <mkouyoumdjian@datanumia.com>
2023-01-28 17:12:18 +0100

    add value in test

--- src/test/resources/sql/insert-contacts.sql
+++ src/test/resources/sql/insert-contacts.sql
@@ -1,2 +1,5 @@
 INSERT INTO contact (id, first_name, last_name, birth_date, address, email, phone_number)
-VALUES('bfdf4b3d-ede1-472b-94ad-96845a5820da', 'firstName1', 'lastName1', '2021-01-01', 'address1', 'test@mail.com', '0100000000');
+VALUES('bfdf4b3d-ede1-472b-94ad-96845a5820da', 'firstName1', 'lastName1', '1980-01-01', 'address1', 'test1@mail.com', '0100000000');
+
+INSERT INTO contact (id, first_name, last_name, birth_date, address, email, phone_number)
+VALUES('181ba7eb-468e-469d-bb0e-4ababc20b426', 'firstName2', 'lastName2', '1990-01-01', 'address2', 'test2@mail.com', '0200000000');
