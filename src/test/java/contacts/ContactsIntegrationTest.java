a2ac48f90d800db47289c4fb87ea9777d5c486c1
mkouyoumdjian <mkouyoumdjian@datanumia.com>
2023-01-31 00:11:12 +0100

    update test

--- src/test/java/contacts/ContactsIntegrationTest.java
+++ src/test/java/contacts/ContactsIntegrationTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,7 +20,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @ActiveProfiles("test")
 @Sql("/sql/insert-contacts.sql")
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class ContactsIntegrationTest {
 
   @Autowired
--- src/test/resources/sql/insert-contacts.sql
+++ src/test/resources/sql/insert-contacts.sql
@@ -1,3 +1,5 @@
+TRUNCATE TABLE contact;
+
 INSERT INTO contact (id, first_name, last_name, birth_date, address, email, phone_number)
 VALUES('bfdf4b3d-ede1-472b-94ad-96845a5820da', 'firstName1', 'lastName1', '1980-01-01', 'address1', 'test1@mail.com', '0100000000');
 
